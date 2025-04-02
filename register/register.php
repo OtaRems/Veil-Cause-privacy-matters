f21ad002785795f822a3fcac9f3859e3124887a4
OtaRems <ancora.antonio07@gmail.com>
2025-04-02 23:31:47 +0200

    Registrazione 1

--- register/register.php
+++ register/register.php
@@ -1,8 +1,27 @@
 <?php
+require "../connessione.php";
+
 $user = $_POST["username"];
 $pass = $_POST["pass"];
 $salt = $_POST["salt"];
 
-$shapass = hash("sha256", $pass, false);
-echo "ciao {$user}, la tua pass è {$pass}<br>il salt usato è {$salt}";
+$smtp = $conn->prepare("SELECT * FROM utenti WHERE username = ?");
+$smtp->bind_param("s", $user);
+$smtp->execute();
+$result = $smtp->get_result();
+
+
+
+if ($result->num_rows == 0) {
+    $passhash = hash("sha256", $pass, false);
+    $smtp = $conn->prepare("INSERT INTO utenti(username,passhash,salt) VALUES (?,?,?)");
+    $smtp->bind_param("sss", $user, $passhash,$salt);
+    $smtp->execute();
+    echo 0;
+}
+else {
+    echo 1;
+}
+
+$smtp->close();
 ?>
